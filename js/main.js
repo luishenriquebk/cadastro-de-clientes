47677f10d0af4c57e0e4a1a32665ae984574d7fb
Luis Henrique <lhkamma@gmail.com>
2023-01-09 14:27:36 -0300

    adicionado clientes tbodyClientes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+// VARIÁVEIS
+let inputNomeCliente = document.querySelector('#inputNomeCliente');
+let inputCpfCliente = document.querySelector('#inputCpfCliente');
+let inputTelefoneCliente = document.querySelector('#inputTelefoneCliente');
+let inputCidadeCliente = document.querySelector('#inputCidadeCliente');
+let tbodyClientes = document.querySelector('#tbodyClientes');
+let bancoDeDadosClientes = [];
+
+// ADICIONAR
+const salvarCliente = () => {
+    let cliente = {
+        nome: inputNomeCliente.value,
+        cpf: inputCpfCliente.value,
+        telefone: inputTelefoneCliente.value,
+        cidade: inputCidadeCliente.value
+    }
+    bancoDeDadosClientes.push(cliente);
+    limparFormulario();
+    criarCliente(cliente);
+    closeModalSaveClient();
+    console.log(bancoDeDadosClientes);
+}
+
+const criarCliente = (cliente) => {
+    let clienteTabela = document.createElement('tr');
+    clienteTabela.innerHTML = 
+    `
+        <td> ${cliente.nome} </td>
+        <td> ${cliente.cpf} </td>
+        <td> ${cliente.telefone} </td>
+        <td> ${cliente.cidade} </td>
+        <td>
+            <button id="btnEditarCliente" onclick="openModalSaveClient()">Editar</button>
+            <button id="btnExcluirCliente" onclick="openModalDeleteClient()">Excluir</button>
+        </td>
+    `
+    tbodyClientes.appendChild(clienteTabela);
+}
+
+// LIMPAR FORMULÁRIO
+const limparFormulario = () => {
+    inputNomeCliente.value = '';
+    inputCpfCliente.value = '';
+    inputTelefoneCliente.value = '';
+    inputCidadeCliente.value = '';
+}
+
 // MODALS
 let modalSaveClient = document.querySelector('#modalSaveClient');
 let modalDeleteClient = document.querySelector('#modalDeleteClient');
